0ec8636efb639092c3715f180d6b4f63e69da27a
RC_Student_lab <RC_Student_lab@labS5HDZL>
2025-04-03 18:23:46 +0000

    Add project files.

---
+++ AudioImage.cs
@@ -0,0 +1,65 @@
+﻿using System.Drawing;
+using System.IO;
+using System.Media;
+using System;
+
+namespace Poe_prog2
+{
+    // Subclass handling ASCII art and audio playback
+    public class AudioImage
+    {
+        public AudioImage()
+        {
+            DisplayArt(); // Displays ASCII image
+            PlayGreetingAudio(); // Plays greeting audio
+        }
+
+        private void DisplayArt()
+        {
+            try
+            {
+                string fullLocation = AppDomain.CurrentDomain.BaseDirectory;
+                string newLocation = fullLocation.Replace("bin\\Debug\\", "");
+                string fullPath = Path.Combine(newLocation, "Logo2.jpg");
+
+                Bitmap image = new Bitmap(fullPath);
+                image = new Bitmap(image, new Size(150, 120));
+
+                for (int height = 0; height < image.Height; height++)
+                {
+                    for (int width = 0; width < image.Width; width++)
+                    {
+                        Color pixelColor = image.GetPixel(width, height);
+                        int gray = (pixelColor.R + pixelColor.G + pixelColor.B) / 3;
+                        char asciiChar = gray > 200 ? '-' : gray > 150 ? '*' : gray > 100 ? 'o' : gray > 50 ? '#' : '@';
+                        Console.Write(asciiChar);
+                    }
+                    Console.WriteLine();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error displaying ASCII art: " + ex.Message);
+            }
+        }
+
+        private void PlayGreetingAudio()
+        {
+            try 
+            {
+                string fullLocation = AppDomain.CurrentDomain.BaseDirectory;
+                string newPath = fullLocation.Replace("bin\\Debug\\", "");
+                string fullPath = Path.Combine(newPath, "greeting.wav");
+
+                using (SoundPlayer play = new SoundPlayer(fullPath))
+                {
+                    play.PlaySync();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error playing audio: " + ex.Message);
+            }
+        }
+    }
+} // End of AudioImage class
